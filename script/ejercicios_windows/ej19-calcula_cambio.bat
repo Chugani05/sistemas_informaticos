10a20f42671ea70bce10805db164e881f61c496e
Rashi <147090292+Chugani05@users.noreply.github.com>
2024-02-20 04:17:44 +0100

    no funciona

--- script/ejercicios_windows/ej19-calcula_cambio.bat
+++ script/ejercicios_windows/ej19-calcula_cambio.bat
@@ -16,29 +16,29 @@ rem * Versión: 2024-02-20 versión 1.0
 rem *
 rem ****************************************
 
-REM Verificar si se proporcionó el precio del artículo como argumento
-if "%~1"=="" (
+rem Verificar si se proporcionó el precio del artículo como argumento
+if "%1"=="" (
     echo Uso: %0 ^<precio_del_articulo^>
     exit /b 1
 )
 
-REM Obtener el precio del artículo del primer argumento
-set "precio_articulo=%~1"
+rem Obtener el precio del artículo del primer argumento
+set "precio_articulo=%1"
 
-REM Solicitar el dinero entregado por el cliente
+rem Solicitar el dinero entregado por el cliente
 set /p dinero_entregado="Indique el dinero pagado: "
 
-REM Calcular el cambio
+rem Calcular el cambio
 set "cambio="
 set /a cambio=dinero_entregado - precio_articulo
 
 echo Se ha comprado un articulo de %precio_articulo% euros y ha pagado %dinero_entregado% euros.
 echo El cambio son %cambio% euros, debe entregar:
 
-REM Definir un array de billetes y su valor
+rem Definir un array de billetes y su valor
 set "billetes=500 200 100 50 20 10 5 2 1"
 
-REM Iterar sobre cada billete para calcular la cantidad a entregar
+rem Iterar sobre cada billete para calcular la cantidad a entregar
 for %%b in (%billetes%) do (
     set /a cantidad=cambio / %%b
     if !cantidad! gtr 0 (
