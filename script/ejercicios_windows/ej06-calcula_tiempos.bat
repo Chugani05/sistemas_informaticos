637324bbb317ac908fbfd48da27dbde04e6a02af
Rashi <147090292+Chugani05@users.noreply.github.com>
2024-02-20 04:09:05 +0100

    done

--- script/ejercicios_windows/ej06-calcula_tiempos.bat
+++ script/ejercicios_windows/ej06-calcula_tiempos.bat
@@ -16,22 +16,22 @@ rem * Versión: 2024-02-20 versión 1.0
 rem *
 rem ****************************************
 
-REM Verificar si se proporcionó el número correcto de argumentos
+rem Verificar si se proporcionó el número correcto de argumentos
 if "%1"=="" (
     echo Por favor, proporciona el numero de segundos como unico argumento.
     exit /b 1
 )
 
-REM Verificar si se proporcionó más de un argumento
+rem Verificar si se proporcionó más de un argumento
 if not "%2"=="" (
     echo Este script solo acepta un unico argumento.
     exit /b 1
 )
 
-REM Convertir el parámetro en un número entero
+rem Convertir el parámetro en un número entero
 set /a segundos=%1
 
-REM Calcular días, horas, minutos y segundos
+rem Calcular días, horas, minutos y segundos
 set /a dias=segundos / 86400
 set /a segundos_restantes=segundos %% 86400
 
