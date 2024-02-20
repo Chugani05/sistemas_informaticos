a570878384e4d0e3de4750ceecac3f2e2b242635
Rashi <147090292+Chugani05@users.noreply.github.com>
2024-02-20 01:10:11 +0100

    Create suma.bat

---
+++ script/ejercicios_windows/suma.bat
@@ -0,0 +1,18 @@
+@echo off
+
+title Suma
+
+rem ****************************************
+rem * Nombre: suma.bat
+rem * Autor: Rashi Chugani Narwani
+rem *
+rem * Objetivo: Sumar dos valores con variables
+rem *
+rem ****************************************
+
+set /a a = 56
+set /a b = 12
+
+set /a resultado = %a% + %b%
+
+echo La suma de %a% + %b% = %resultado%
